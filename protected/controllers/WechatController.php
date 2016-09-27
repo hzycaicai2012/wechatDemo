5f7bdb1bf16e5131bc495e9decb3886b1b51686e
hzycaicai2012 <hzycaicai@163.com>
2016-09-27 22:07:45 +0800

    add token
    add token

--- protected/controllers/WechatController.php
+++ protected/controllers/WechatController.php
@@ -5,10 +5,11 @@
  * Date: 2016/9/27
  * Time: 21:52
  */
+define("TOKEN", "hzycaicai");
+
 class WechatController extends Controller
 {
     public function actionIndex() {
-
         $check = $this->checkSignature();
         if ($check) {
             echo isset($_GET["echostr"]) ? $_GET["echostr"] : "";
@@ -19,23 +20,24 @@ class WechatController extends Controller
 
     private function checkSignature()
     {
-        try {
-            $signature = $_GET["signature"];
-            $timestamp = $_GET["timestamp"];
-            $nonce = $_GET["nonce"];
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
 
-            $token = TOKEN;
-            $tmpArr = array($token, $timestamp, $nonce);
-            sort($tmpArr, SORT_STRING);
-            $tmpStr = implode( $tmpArr );
-            $tmpStr = sha1( $tmpStr );
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
 
-            if( $tmpStr == $signature ){
-                return true;
-            }else{
-                return false;
-            }
-        } catch (Exception $e) {
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
             return false;
         }
     }
