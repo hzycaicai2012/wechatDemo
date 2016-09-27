5a529f9fe47ba84282f6fa8f404820788d331a66
Your Name <you@example.com>
2016-09-27 21:56:07 +0800

    add config

--- protected/config/main.php
+++ protected/config/main.php
@@ -39,16 +39,15 @@ return array(
 		),
 
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+                        'showScriptName' => false,
 			'rules'=>array(
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
