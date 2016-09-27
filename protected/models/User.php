7342de88338d6655c29513539aa9d928d2fc04de
hzycaicai2012 <hzycaicai@163.com>
2016-09-27 21:39:32 +0800

    test model
    add user model

--- protected/models/User.php
+++ protected/models/User.php
@@ -1,1 +1,21 @@
+<?php
 
+class User extends CActiveRecord
+{
+    /**
+     * @param string $className
+     * @return static
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string
+     */
+    public function tableName()
+    {
+        return 'wc_user';
+    }
+}
